75c81148403d8bdfcb36a52feddff638ab97b0cd
vivekananda69 <vivekanandabandapu1@gmail.com>
2025-05-13 19:04:57 +0530

    registration backend/api

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './assets/css/style.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Main from './components/Main'
+import Main from './components/main'
 
 function App() {
 
