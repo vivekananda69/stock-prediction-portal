1f61501196fac2b711b917f05f4e9f739b0dcf19
vivekananda69 <vivekanandabandapu1@gmail.com>
2025-05-13 18:07:27 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+    <footer className='footer py-3 my-3'>
+    <hr className='border-bottom' />
+    <p className="text-light text-center">&copy; 2025 - built with ❤️ Vivekananda</p>
+    </footer>
+   </>
+  )
+}
+
+export default Footer
