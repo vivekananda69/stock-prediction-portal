f21c1a591eaea20005db8b694614be8c24ba4728
vivekananda69 <vivekanandabandapu1@gmail.com>
2025-05-17 16:42:28 +0530

    integrating machine learning model with django and react application

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -1,53 +1,63 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_BACKEND_BASE_API;
 
-const baseURL = import.meta.env.VITE_BACKEND_BASE_API
 const axiosInstance = axios.create({
     baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     }
-})
-
+});
 
 // Request Interceptor
 axiosInstance.interceptors.request.use(
-    function(config){
-        const accessToken = localStorage.getItem('accessToken')
-        if(accessToken){
-            config.headers['Authorization'] = `Bearer ${accessToken}`
+    function (config) {
+        const accessToken = localStorage.getItem('accessToken'); 
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
         }
         return config;
     },
-    function(error){
+    function (error) {
         return Promise.reject(error);
     }
-)
+);
 
 // Response Interceptor
 axiosInstance.interceptors.response.use(
-    function(response){
+    function (response) {
         return response;
     },
-    // Handle failed responses
-    async function(error){
+    async function (error) {
         const originalRequest = error.config;
-        if(error.response.status === 401 && !originalRequest.retry){
-            originalRequest.retry = true;
-            const refreshToken = localStorage.getItem('refreshToken')
-            try{
-                const response = await axiosInstance.post('/token/refresh/', {refresh: refreshToken})
-                localStorage.setItem('accessToken', response.data.access)
-                originalRequest.headers['Authorization'] = `Bearer ${response.data.access}`
-                return axiosInstance(originalRequest)
-            }catch(error){
-                localStorage.removeItem('accessToken')
-                localStorage.removeItem('refreshToken')
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            const refreshToken = localStorage.getItem('refreshToken'); 
+            if (!refreshToken) {
+                return Promise.reject(error);
+            }
+
+            try {
+                const response = await axios.post(`${baseURL}/token/refresh/`, {
+                    refresh: refreshToken
+                });
+
+                const newAccessToken = response.data.access;
+                localStorage.setItem('accessToken', newAccessToken); 
+
+                originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                return axiosInstance(originalRequest);
+            } catch (refreshError) {
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+                return Promise.reject(refreshError);
             }
         }
+
         return Promise.reject(error);
     }
-)
-
+);
 
 export default axiosInstance;
